3e14e78e1a9118f7ac84fb6f7b64bae4105f8225
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2025-01-20 17:14:55 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "./public")));
+
+const mustache = require("mustache-express");
+app.engine("mustache", mustache());
+app.set("view engine", "mustache");
+
+app.post("/processForm", function (req, res) {
+  let userName = req.body.firstName;
+  res.render("formResults", {
+    title:'Input from form',
+    name: userName,
+    surname:req.body.surname
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port: 3000");
+});
