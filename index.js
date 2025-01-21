09a72fc3adc5ccbfb41854342dd3fe83c0a84ce3
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2025-01-21 15:54:09 +0000

    basic client and serveside form examples using express

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ const mustache = require("mustache-express");
 app.engine("mustache", mustache());
 app.set("view engine", "mustache");
 
-app.get("/serverForm2",function (req, res) {
-  res.render("form", 
-    {
-      title:'Input from form',
-      name: "",
-      surname:""
-    });
-  });
-
 app.post("/processForm", function (req, res) {
   let userName = req.body.firstName;
   res.render("formResults", {
@@ -25,18 +16,6 @@ app.post("/processForm", function (req, res) {
     surname:req.body.surname
   });
 });
-
-
-app.post("/processForm2", function (req, res) {
- let userName= req.body.firstName
- console.log(userName)
-  res.render("form", {
-    title:'Input from form',
-    name: req.body.firstName,
-    surname:req.body.surname
-  });
-});
-
 
 app.listen(3000, () => {
   console.log("Server listening on port: 3000");
