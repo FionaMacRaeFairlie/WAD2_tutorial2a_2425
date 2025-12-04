d1954bbb86975b5f3821321a19792c37289608af
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2025-12-04 16:59:53 +0000

    updated example to esm

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const express = require("express");
-const path = require("path");
+// app.js (ES Modules)
+
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import mustacheExpress from "mustache-express";
+
+// Recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+// Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: false }));
+
+// Serve static files from ./public
 app.use(express.static(path.join(__dirname, "./public")));
 
-const mustache = require("mustache-express");
-app.engine("mustache", mustache());
+// Mustache template engine
+app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 
 app.post("/processForm", function (req, res) {
-  let userName = req.body.firstName;
+  const userName = req.body.firstName;
   res.render("formResults", {
-    title:'Input from form',
+    title: "Input from form",
     name: userName,
-    surname:req.body.surname
+    surname: req.body.surname,
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port: 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
 });
